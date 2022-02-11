3429e69b8878dd383da459b42db08a52127511b3
Sam-Ofiaza <98000654+Sam-Ofiaza@users.noreply.github.com>
2022-02-11 11:14:13 -0600

    Update phase1.fixed.v

--- phase1.fixed.v
+++ phase1.fixed.v
@@ -9,56 +9,61 @@
     end
 endmodule*/
 
-module f8(input w, x, y, z, output o);
-
+module f8(input w, x, y, z, output o); // "Class" f8
+    
     wire a, b, c, d;
-    and a1(a, ~w, y);
-    and a2(b, x, ~y);
-    and a3(c, ~w, ~z);
-    and a4(d, w, ~y, z);
-    or o1(o, a, b, c, d);
+    and a1(a, ~w, y); // w'y
+    and a2(b, x, ~y); // xy'
+    and a3(c, ~w, ~z); // w'z'
+    and a4(d, w, ~y, z); // wy'z
+    or o1(o, a, b, c, d); // w'y + xy' + w'z' + wy'z
 
 endmodule
 
-module f9(input w, x, y, z, output o);
+module f9(input w, x, y, z, output o); // "Class" f9
 
-    wire a, b, c, d;
-    and a1(a, x, z);
+    wire a, b, c, d; // Wires that connect the gates for this circuit together
+    and a1(a, x, z); // and is the "class," a1 is the instantiation, the first parameter a is the output, the rest (x, z) are inputs
     and a2(b, w, x, ~y);
     and a3(c, w, ~y, z);
     and a4(d, ~x, y, ~z);
-    or o1(o, a, b, c, d);
+    or o1(o, a, b, c, d); // Same parameter ordering for or gates
 
 endmodule
 
-module testbench;
-    integer i;
-    wire [9:0] f;
-    reg w, x, y, z;
+module testbench; // Runner/stimulus
+    integer i; // Can't instantiate ints/registers in for loop, so do it here
+    
+    wire [9:0] f; // A wire bus - has 9 to 0 bits, total 10 bits to connect input/output to
+                  // "f" will be used to store the output of every module to display on the terminal
+    
+    reg w, x, y, z; // Registers can store values, wires can only connect circuits but not store anything
+                    // These will be used to store the inputs used for the modules
 
-    f8 eq8(.w(w), .x(x), .y(y), .z(z), .o(f[8]));
-    f9 eq9(.w(w), .x(x), .y(y), .z(z), .o(f[9]));
+    f8 eq8(.w(w), .x(x), .y(y), .z(z), .o(f[8])); // Instantiate modules
+    f9 eq9(.w(w), .x(x), .y(y), .z(z), .o(f[9])); // .w(w) means take module f8's input w and connect it to this module's register w
 
-    initial
-    begin
+    initial // initial blocks only run once but if you put multiple initial blocks then they will run in parallel
+    begin // Equivalent to '{'
         $display ("|Row|w|x|y|z|f8|f9|");
         $display ("|===+=+=+=+=+==+==|");
 
-        for(i = -1; i < 16; i = i + 1)
+        for(i = -1; i < 16; i = i + 1) // No ++ operator in Verilog
         begin
-            w = (i/8)%2;
+            w = (i/8)%2; // Each of these will only be 1 bit, though I think you can store bigger values in registers
             x = (i/4)%2;
             y = (i/2)%2;
             z = (i/1)%2;
 
-            #10;
+            #10; // Delay by 10 time units - used to make sure calculation completes before "print" statement and not during it
+                 // Time units can be changed at the very top of the file, here is an example: `timescale 1ps/1ps
 
-            $monitor ("| %2d|%1d|%1d|%1d|%1d| %1d| %1d|", i, w, x, y, z, f[8], f[9]);
+            $monitor ("| %2d|%1d|%1d|%1d|%1d| %1d| %1d|", i, w, x, y, z, f[8], f[9]); // Similar syntax to C print statements, will display if any values change
         end
 
         #10;
-        $finish;
+        $finish; // End simulation
 
-    end
+    end // Equivalent to '}'
 
 endmodule
